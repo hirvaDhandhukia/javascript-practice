8b6c15a26db0d63c8e9e8178f7857c89460b8208
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-01 01:46:07 +0530

    add: better way to handle this by looping through btns

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,13 +1,46 @@
-// getting elements form the button and the h1 tag
-var value = document.getElementById('num');
-const decrease = document.getElementById('decrease');
-const reset = document.getElementById('reset');
-const increase = document.getElementById('increase');
+// // getting elements form the button and the h1 tag
+// var value = document.getElementById('num');
+// const decrease = document.getElementById('decrease');
+// const reset = document.getElementById('reset');
+// const increase = document.getElementById('increase');
 
+// let count = 0;
+
+// // adding onclick event listner to the increase button
+// increase.addEventListener("click", () => {
+//     count++;
+//     value.innerHTML = count;
+// });
+
+// generating all different functions is not efficient way to do this
+// so i'm using a new variable with document.querySelectorAll
+var value = document.getElementById('num');
+const btns = document.querySelectorAll('.btn');
 let count = 0;
 
-// adding onclick event listner to the increase button
-increase.addEventListener("click", () => {
-    count++;
-    value.innerHTML = count;
+// using a for loop here for iterating through all 3 btns
+btns.forEach((btn) => {
+    btn.addEventListener("click", (k) => {
+        // using a JS event property called currentTarget
+        const styles = k.currentTarget.classList;
+        if(styles.contains("decrease")) {
+            count --;
+        } else if (styles.contains("increase")) {
+            count ++;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        } else if (count < 0) {
+            value.style.color = "red";
+        } 
+        else if (count == 0) {
+            value.style.color = "#222";
+        }
+
+        value.textContent = count;
+        // value.innerHTML = count;
+    });
 });
