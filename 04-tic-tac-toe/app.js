a0e8b04994bd736c0d6ed558a013c34990d6198f
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-31 01:29:27 +0530

    checkWin function done i.e. will identify when and which player won

--- 04-tic-tac-toe/app.js
+++ 04-tic-tac-toe/app.js
@@ -1,11 +1,30 @@
 // console.log("welcome to my first JS game");
 let turn = "X";
+let gameover = false;
+
 const changeTurn = () => {
     return turn === "X"?"O" : "X";
 }
 
 const checkWin = () => {
-
+    let boxText = document.getElementsByClassName('boxText');
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    wins.forEach(e => {
+        if((boxText[e[0]].innerText === boxText[e[1]].innerText) && (boxText[e[1]].innerText === boxText[e[2]].innerText) && (boxText[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxText[e[0]].innerText + " Won";
+            
+            gameover = true;
+        }
+    })
 };
 
 
@@ -20,7 +39,10 @@ Array.from(boxes).forEach(element => {
             turn = changeTurn();
             checkWin();
 
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            if(!gameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     })
 })
+
