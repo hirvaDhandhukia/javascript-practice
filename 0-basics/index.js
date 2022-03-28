0b9402a0b82c69561f222efce1c88dc02c8ba4a0
Hirva Dhandhukia <hirva.dhandhukia@gmail.com>
2022-03-29 02:54:47 +0530

    hoisting, function call in js

---
+++ 0-basics/index.js
@@ -0,0 +1,32 @@
+// import "./styles.css";
+
+
+// Hoisting in JS
+// getName();
+// console.log(x);
+
+// getName();
+// console.log(getName);
+
+// var x = 7;
+
+// var getName = () => {
+//   console.log("Hirva Dhandhukia");
+// }
+
+
+
+// functions
+var x=1;
+a(); 
+b();
+console.log(x);
+
+function a() {
+    var x = 10;
+    console.log(x);
+}
+function b() {
+    var x = 100;
+    console.log(x);
+}
