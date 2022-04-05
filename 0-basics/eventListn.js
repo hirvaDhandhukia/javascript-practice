eabb279bd69b3ac05540247baf8e59a65ce67a2a
Hirva Dhandhukia <hirva.dhandhukia@gmail.com>
2022-04-05 14:19:27 +0530

    blocking main thread

--- 0-basics/eventListn.js
+++ 0-basics/eventListn.js
@@ -1,14 +1,38 @@
-// click handler
+// // click handler
+
+// // using closure 
+// function attachEventListners() {
+//     document.getElementById("clickMe")
+//     .addEventListener("click", function xyz() {
+//         console.log("You clicked btn", ++count)
+//     })
+//     let count = 0;
+// }
+
+// attachEventListners();
 
 
-// using closure 
-function attachEventListners() {
-    document.getElementById("clickMe")
-    .addEventListener("click", function xyz() {
-        console.log("You clicked btn", ++count)
-    })
-    let count = 0;
+
+
+// blocking the main thread (setTimeout issues)
+console.log("Start");
+
+function cb() {
+    console.log("Callback function executed");
 }
+setTimeout(cb,0);
 
-attachEventListners();
+// setTimeout(function cb() {
+//     console.log("Callback function executed");
+// }, 5000);
 
+console.log("End");
+
+// // million
+// let startDate = new Date().getTime();
+// let endDate = startDate;
+// while(endDate < startDate + 10000) {
+//     endDate = new Date().getTime();
+// }
+
+// console.log("while loop expires");
